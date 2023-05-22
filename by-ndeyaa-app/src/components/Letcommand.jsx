ba8dd645992b65f8d2203cba45312bdab90d2eb2
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-05-23 00:35:26 +0200

    update

--- by-ndeyaa-app/src/components/Letcommand.jsx
+++ by-ndeyaa-app/src/components/Letcommand.jsx
@@ -1,14 +1,56 @@
 import React from "react";
+import { useState } from "react";
 import tabs from "./tabs";
+import axios from 'axios';
 
 export default function Letcommand() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+      });
+    
+      const handleChange = (event) => {
+        setFormData({
+          ...formData,
+          [event.target.name]: event.target.value
+        });
+      };
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        // Envoyer les données du formulaire via une méthode POST
+        fetch('http://localhost:8000/api/send-email', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log('E-mail envoyé avec succès !');
+          })
+          .catch(error => {
+            console.error('Erreur lors de l\'envoi de l\'e-mail:', error);
+          });
+      };
+
+      /*await fetch('http://localhost:8000/commander', { method: 'POST' }).then((response) => response.json())
+        .then((promise) => {
+            //dishes = promise
+            console.log(promise)
+            alert("commande bien envoyé");
+        })*/
+
     return (
         <section className="common-section" id="let-command" style={{ padding: "" }}>
             <div className="title-section">
                 <h1>Let's go commander !</h1>
                 <img className="img-illustration" src="images/icons/bon-de-commande.png" alt="img-icon" />
             </div>
-            <div className="command-form" style={{ margin: "0 10%", display:"grid", justifyContent: "" }}>
+            <form className="command-form" onSubmit={handleSubmit} style={{ margin: "0 10%", display:"grid", justifyContent: "" }}>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" name="lastName" id="floatingInput" placeholder="Sene" />
                     <label for="floatingInput">Nom</label>
@@ -36,13 +78,13 @@ export default function Letcommand() {
                     })}
                 </select>
                 <button 
-                type="button" class="btn btn-outline-success" 
+                type="submit" class="btn btn-outline-success" 
                 style={{
                     margin: "4% 43%"
                 }}
                 
                 > Commander </button>
-            </div>
+            </form>
         </section>
     )
 }
--- by-ndeyaa-app/src/mailer.js
+++ by-ndeyaa-app/src/mailer.js
@@ -1,45 +1,66 @@
-const expresss = require('express');
+const express = require('express');
+require('dotenv').config();
 const app = express();
+app.use(express.json());
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
-var firstName = req.body.firstName;
-var lastName = req.body.lastName;/**/
-var tel = req.body.tel;
-var adresse = req.body.adresse; 
-var article = req.body.article;
 
-var client = `Nom : ${firstName} \n Prenom : ${lastName} \n Tel : ${tel} Prenom : ${adresse}`;
-var article = `Article : ${article}`;
+//alert('Votre commande a été bien passé\n.On vous contactera bientôt pour les modalités de livraison! :)')
+PORT = 8000
+app.listen(PORT, () => {
+    console.log(`Server Started at http://localhost:${PORT}`)
+})
 
-var message = "Nouvele commande\n\n" + client + article;
-export default function commander(){
-    let transporter = nodemailer.createTransport({  
-        host: 'smtp.gmail.com',
-        port: 587, // 587 -> TLS & 465 -> SSL
-        auth: {  
-          user: 'mayelthiam81@gmail.com', // email de votre votre compte google
-          pass: 'yzwfjryqrnqwknfg' // password de votre compte google
-        }  
-    });
+app.get('/', (req, res)=>{
+    res.send({message: "bienvenue dans le serveur de by-ndeyaa"});
+})
 
-    let mail = {  
-        from: 'mayelthiam81@gmail.com',  
-        to: 'papemayel1515@gmail.com',  
-        subject: 'Nouvelle commande !',  
-        text: message
-        // on peut remplacer l'attribut `text`par `html`si on veut que le cors de notre email supporte le HTML
-        // html:  '<h1>This email use html</h1>'
-      };
+app.post('/api/commander', (req, res) => {
+    // recuperer les informations du formulaire
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var tel = req.body.tel;
+    var adresse = req.body.adresse; 
+    var article = req.body.article;
 
-    transporter.sendMail(mail, (error, info) => {  
-        if (error) {  
-            console.log(error);  
-        } else {  
-            console.log('Email: ' + info.response);  
-        }  
-    });
-    //alert('Votre commande a été bien passé\n.On vous contactera bientôt pour les modalités de livraison! :)')
-}
+    var client = `Nom : ${firstName} \nPrenom : ${lastName} \nTel : ${tel} \nAdresse : ${adresse}\n`;
+    var article = `Article : ${article}\n`;
+    var messageMail = "Nouvelle commande de : \n\n" + client + article;
 
-commander();
+    // faire l'envoi des données par mail
+    if(!(firstName==="" || lastName==="" || adresse==="" || tel==="")){
+        let transporter = nodemailer.createTransport({  
+            host: 'smtp.gmail.com',
+            port: 587, // 587 -> TLS & 465 -> SSL
+            auth: {  
+              user: 'mayelthiam81@gmail.com', // email de votre votre compte google
+              pass: 'yzwfjryqrnqwknfg' // password de votre compte google
+            }  
+        });
+    
+        let mail = {  
+            from: 'mayelthiam81@gmail.com',  
+            to: 'papemayel1515@gmail.com',  
+            subject: 'Nouvelle commande !',  
+            text: messageMail
+            // on peut remplacer l'attribut `text`par `html`si on veut que le cors de notre email supporte le HTML
+            // html:  '<h1>This email use html</h1>'
+          };
+    
+        transporter.sendMail(mail, (error, info) => {  
+            if (error) {  
+                console.log(error);  
+                res.send({message: "error"});
+            } else {  
+                console.log('Email: ' + info.response);  
+                res.send({message: "mail sent successfully"});
+            }  
+        });
+        
+    } else{
+        res.send({message: "important field empty !"})
+    }
+
+})
