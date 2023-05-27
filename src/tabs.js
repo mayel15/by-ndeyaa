63cb205b3b27020cf8a0aa730726059f2cfa9813
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-05-27 16:15:49 +0200

    last fix and update

--- src/tabs.js
+++ src/tabs.js
@@ -3,61 +3,61 @@ const tabs = [
         key: 1,
         img: "images/tabs/welcome.jpeg",
         title: "DUO",
-        price: "20000 CFA"
+        price: "20000CFA"
     },
     {
         key: 2,
         img: "images/tabs/img2.jpeg",
         title: "Tableau 2",
-        price: "13000 CFA"
+        price: "13000CFA"
     },
     {
         key: 3,
         img: "images/tabs/img3.jpeg",
         title: "BLACK&GOLD",
-        price: "13000 CFA"
+        price: "13000CFA"
     },
     {
         key: 4,
         img: "images/tabs/img4.jpeg",
         title: "SUR_VERRE",
-        price: "13000 CFA"
+        price: "13000CFA"
     },
     {
         key: 5,
         img: "images/tabs/img5.jpeg",
         title: "SKY",
-        price: "13000 CFA"
+        price: "13000CFA"
     },
     {
         key: 6,
         img: "images/tabs/img6.jpeg",
         title: "Tableau 1",
-        price: "13000 CFA"
+        price: "13000CFA"
     },
     {
         key: 7,
         img: "images/tabs/img7.jpeg",
         title: "Tableau 1",
-        price: "13000 CFA"
+        price: "13000CFA"
     },
     {
         key: 8,
         img: "images/tabs/img8.jpeg",
         title: "GREY",
-        price: "15000 CFA"
+        price: "15000CFA"
     },
     {
         key: 9,
         img: "images/tabs/img9.jpeg",
         title: "Tableau 1",
-        price: "13000 CFA"
+        price: "13000CFA"
     },
     {
         key: 10,
         img: "images/tabs/img10.jpeg",
         title: "Tableau 1",
-        price: "13000 CFA"
+        price: "13000CFA"
     }
 ]
 export default tabs;
