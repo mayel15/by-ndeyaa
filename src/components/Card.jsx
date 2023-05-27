66d4f3f80e0bb29a2ccd96208de4acd64a959abe
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-05-27 16:42:30 +0200

    fix size of text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 import './styles.css';
 export default function Card(props) {
     return (
-        <div className="tab-card col-lg-6 col-md-2">
+        <div className="tab-card col-lg-4 col-md-2">
             <img className="img-fluid" src={props.img} alt="img-tab" />
             <div className="card-info">
                 <p>{props.title}</p>
