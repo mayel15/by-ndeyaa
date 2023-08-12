75ce4698c7efd33217d82061fdfb357dd13308d7
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-08-12 13:37:06 +0200

    fix width testimonial

--- src/components/Testicard.jsx
+++ src/components/Testicard.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 export default function Testicard(props){
     return (
         <div className="testi-card">
-            <img className="img-user" style={{height: "8rem", width: "8rem", borderRadius: "30%"}}src={props.img}  alt="img-user"/>
+            <img className="img-user" style={{height: "8rem", width: "8rem", borderRadius: "30%"}} src={props.img}  alt="img-user"/>
             <img className="img-com" style={{height: "8rem", width: "8rem"}} src="images/icons/la-communication.png" alt="la-communication"/>
             <p>{props.text}</p>
         </div>
