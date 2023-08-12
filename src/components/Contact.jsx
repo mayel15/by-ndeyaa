28622ff5cade158f8f4577458c82c4deb9c8adb5
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-08-12 12:58:24 +0200

    fix viewport

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact(){
                     <h2><i class="fa-sharp fa-solid fa-location-dot"></i>  Zone de Captage, Dakar, Sénégal</h2>
                 </div>
                 <div className="map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1929.3565918114678!2d-17.444116461185825!3d14.728800996443209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xec172d2b94a61cb%3A0x3e408f9ca3feb9ee!2zWm9uZSBkZSBjYXB0YWdlLCBEYWthciwgU8OpbsOpZ2Fs!5e0!3m2!1sfr!2sfr!4v1684763117814!5m2!1sfr!2sfr" title="localisation" width="600" height="450" style={{ border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    {/*<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1929.3565918114678!2d-17.444116461185825!3d14.728800996443209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xec172d2b94a61cb%3A0x3e408f9ca3feb9ee!2zWm9uZSBkZSBjYXB0YWdlLCBEYWthciwgU8OpbsOpZ2Fs!5e0!3m2!1sfr!2sfr!4v1684763117814!5m2!1sfr!2sfr" title="localisation" width="600" height="450" style={{ border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>*/}
                 </div>
             </div>
         </section>
