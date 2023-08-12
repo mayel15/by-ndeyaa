e3d87b83a14e2b68574636f754c0a07e78fd759e
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-08-12 13:10:06 +0200

    fix viewport

--- src/components/Testicard.jsx
+++ src/components/Testicard.jsx
@@ -4,7 +4,7 @@ export default function Testicard(props){
     return (
         <div className="testi-card">
             <img className="img-user" style={{height: "8rem", width: "8rem", borderRadius: "30%"}}src={props.img}  alt="img-user"/>
-            <img className="img-com" style={{height: "8rem", width: "8rem"}} src="images/icons/la-communication.png" alt="la-communication"/>
+            {/*<img className="img-com" style={{height: "8rem", width: "8rem"}} src="images/icons/la-communication.png" alt="la-communication"/>*/}
             <p>{props.text}</p>
         </div>
     )
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import './styles.css';
 export default function Welcome(){
     var welcomeText = "By_Ndeyaa est une petite entreprise qui vous propose des tableaux pour votre intérieur. Ces tableaux apportent une petite touche de rappel islamique sur votre décoration."
     return(
-        <section id="welcome" >
+        <section id="welcome" style={{width: "100%"}} >
             <img class="img-welcome img-fluid" src="images/tabs/welcome.jpeg" alt="img-welcome" />
             <div class="word-welcome">
                 <h1>Bienvenue chez Ndeyaa !</h1>
